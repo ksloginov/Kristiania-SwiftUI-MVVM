e53b1392122cc3027d6c3d469d237a4aefc2a895
Konstantin Loginov <loginov.k@gmail.com>
2021-08-05 13:24:28 +0200

    Step 5: adding a basic AppDataService

---
+++ SwiftUI+MVVM/AppDataService.swift
@@ -0,0 +1,19 @@
+//
+//  AppDataService.swift
+//  SwiftUI+MVVM
+//
+//  Created by Konstantin Loginov on 05.08.2021.
+//
+
+import Foundation
+
+class AppDataService {
+    func getUsers(completion: @escaping ([User]) -> Void) {
+        completion([
+            User(id: 1, name: "Konstantin"),
+            User(id: 2, name: "Adam"),
+            User(id: 3, name: "Eva")
+        ])
+    }
+}
+
