766627589a7635938669d76750afc1b4b6689626
Konstantin Loginov <loginov.k@gmail.com>
2021-08-05 13:14:34 +0200

    Step 1: renaming ContentView to UsersView

--- SwiftUI+MVVM/UsersView.swift
+++ SwiftUI+MVVM/UsersView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct UsersView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct UsersView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        UsersView()
     }
 }
