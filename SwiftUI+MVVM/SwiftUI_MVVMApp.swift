39e9fb72a23428c9eb06903e65c44e7fa5b053f2
Konstantin Loginov <loginov.k@gmail.com>
2021-08-05 13:12:20 +0200

    Initial Commit

---
+++ SwiftUI+MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVMApp.swift
+//  SwiftUI+MVVM
+//
+//  Created by Konstantin Loginov on 05.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
