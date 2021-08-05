6a3ab4f0f3f16bbc74f3ef47b9f5a6591720a2b9
Konstantin Loginov <loginov.k@gmail.com>
2021-08-05 13:20:08 +0200

    Step 3: Adding a User struct

---
+++ SwiftUI+MVVM/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  SwiftUI+MVVM
+//
+//  Created by Konstantin Loginov on 05.08.2021.
+//
+
+import Foundation
+
+struct User: Identifiable {
+    let id: Int
+    let name: String
+}
