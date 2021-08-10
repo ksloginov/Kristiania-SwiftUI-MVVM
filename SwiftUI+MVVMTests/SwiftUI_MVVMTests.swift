37cfdb7a2e4d9aae094052d09f3bedb8ef8f96e0
Konstantin Loginov <loginov.k@gmail.com>
2021-08-10 10:45:22 +0200

    Step 10: Adding a Mocked version of the DataService to serve in.tests

--- SwiftUI+MVVMTests/SwiftUI_MVVMTests.swift
+++ SwiftUI+MVVMTests/SwiftUI_MVVMTests.swift
@@ -8,26 +8,21 @@
 import XCTest
 @testable import SwiftUI_MVVM
 
+class MockedDataService: DataService {
+    func getUsers(completion: @escaping ([User]) -> Void) {
+        completion([User(id: 300, name: "Leroy")])
+    }
+}
+
 class SwiftUI_MVVMTests: XCTestCase {
+    
+    var viewModel: UsersView.UsersViewModel!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewModel = .init(dataService: MockedDataService())
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
